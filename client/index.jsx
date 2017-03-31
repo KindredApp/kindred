e76a7b490671f1b500122f84e693be3757a87f5d
Zane Ravenholt <zravenholt@gmail.com>
2017-03-31 15:56:14 -0700

    minor tweaks

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import 'babel-polyfill';
 import App from './containers/app.jsx';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render((<App />), document.getElementById('root'));
