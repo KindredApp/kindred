56265311174576cb23339c64c43cae9663bd21af
Zane Ravenholt <zravenholt@gmail.com>
2017-03-31 16:46:46 -0700

    merge go functionality with existing front end

--- client/index.jsx
+++ client/index.jsx
@@ -2,5 +2,8 @@ import 'babel-polyfill';
 import App from './containers/app.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+
+
 
 ReactDOM.render((<App />), document.getElementById('root'));
