22e32818ae8e2de4f4d09c671be7b04e8b1afb1a
nowreen <chowdhry.n@gmail.com>
2017-04-03 21:05:51 -0700

    Implement basic video and chat with user form

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ExampleList from '../containers/exampleList.js';
 import ExampleClicked from '../containers/exampleClicked.js';
+import Video from './video.jsx';
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <h1>APP SAYS HELLO</h1>
       <ExampleList />
       <ExampleClicked />
+      <Video />
     </div>
     );
   }
--- client/components/video.jsx
+++ client/components/video.jsx
@@ -1,6 +1,9 @@
-import React, {Component} from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PubNub from 'pubnub';
+import pubnubConfig from '../../pubnubConfig.js';
 
-class Main extends React.Component {
+export default class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -16,8 +19,8 @@ class Main extends React.Component {
     };
 
     this.pubnub = new PubNub({
-        publishKey: 'pub-c-cf1aafc6-8870-4bd2-b7f0-261fe8871836',
-        subscribeKey: 'sub-c-9d8fe528-1422-11e7-a5a9-0619f8945a4f',
+        publishKey: pubnubConfig.publishKey,
+        subscribeKey: pubnubConfig.subscribeKey,
         ssl: true
     });
 
@@ -80,9 +83,9 @@ class Main extends React.Component {
   login(e) {
     e.preventDefault();
     let phone = window.phone = PHONE({
-        number: this.state.caller || "Anonymous", // listen on username line else Anonymous
-        publish_key: 'pub-c-cf1aafc6-8870-4bd2-b7f0-261fe8871836',
-        subscribe_key: 'sub-c-9d8fe528-1422-11e7-a5a9-0619f8945a4f',
+        number: this.state.caller || "Anonymous",
+        publish_key: pubnubConfig.publishKey,
+        subscribe_key: pubnubConfig.subscribeKey,
         ssl: true
     });	
     phone.ready(() => {
@@ -92,7 +95,6 @@ class Main extends React.Component {
     phone.receive((session) => {
         session.connected((session) => {
           console.log('Connected. Caller: ', this.state.caller);
-          // this.refs.video.innerHTML = session.video;
           video_out.appendChild(session.video);
         });
         session.ended((session) => {
@@ -102,21 +104,6 @@ class Main extends React.Component {
   }
 
   makeCall() {
-    // let phone = window.phone = PHONE({
-    //   number: this.state.caller,
-    //   publish_key: 'pub-c-cf1aafc6-8870-4bd2-b7f0-261fe8871836',
-    //   subscribe_key: 'sub-c-9d8fe528-1422-11e7-a5a9-0619f8945a4f'
-    // });
-    // phone.ready(() => {console.log('phone ready')});
-    // phone.receive((session) => {
-    //   session.connected((session) => {
-    //     console.log('connected');
-    //     this.refs.video.innerHTML = session.video;
-    //   });
-    //   session.ended((session) => {
-    //     this.refs.video.innerHTML = '';
-    //   });
-    // });
     phone.dial(this.state.callee);
   }
 
@@ -148,5 +135,3 @@ class Main extends React.Component {
       );
   }
 }
-
-ReactDOM.render(<Main/>, document.getElementById('main'));
