3fd00187c3d8eeda4e09474947f31779cc870d9a
nowreen <chowdhry.n@gmail.com>
2017-04-10 12:12:35 -0700

    Add route protection on router and render user thumbnail video once

--- client/components/root.jsx
+++ client/components/root.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
-import { HashRouter as Router, Route, Link } from 'react-router-dom'; 
+import { HashRouter as Router, Route, Link, Redirect } from 'react-router-dom'; 
 import { Form } from 'antd';
 import App from './app.jsx';
 import AboutPage from './aboutpage.jsx';
@@ -10,10 +10,49 @@ import Video from '../containers/video/video.jsx';
 import Survey from '../containers/account/survey.jsx';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
-import '../styles/index.css'
+import Cookies from 'js-cookie';
+import axios from 'axios';
+import '../styles/index.css';
 
 const SignUpForm = Form.create()(SignUp);
 const LoginForm = Form.create()(Login);
+
+const PrivateRoute = ({component, user}) => (
+  <Route {...user} render={(user) => (
+    authenticate(user) ? 
+      React.createElement(component, user) : 
+      <Redirect to='/login'/>
+  )}/>
+);
+
+const checkToken = () => {
+  let cookie = Cookies.getJSON();
+  for (let key in cookie) {
+    if (key !== 'pnctest') {
+      return (axios.post('/api/tokenCheck', {
+        Username: cookie[key].Username,
+        Token: cookie[key].Token
+      }).then((response) => {
+        console.log('check token repsonse', response);
+        return response.data;
+      }));
+    } else {
+      return false;
+    }
+  }
+};
+
+const authenticate = (props) => {
+  if (!props.user) {
+    if (!checkToken()) {
+      return false;
+    } else {
+      return true;
+    }
+  } else {
+    return true;
+  }
+};
 
 class Root extends Component {
   constructor (props) {
@@ -29,7 +68,7 @@ class Root extends Component {
             <Route exact path="/" component={App} />
             <Route path="/signup" component={SignUpForm} />
             <Route path="/login" component={LoginForm} />
-            <Route path="/video" component={Video} />
+            <PrivateRoute path="/video" component={Video} user={this.props.user} />
             <Route path="/survey" component={Survey} />
             <Route path="/aboutus" component={AboutPage} />
           </div>
