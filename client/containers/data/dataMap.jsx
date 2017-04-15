5d8409c96858d67d9b44c858ca34bbcd6c226ac5
Lindsay Anchors <lindsay.anchors@gmail.com>
2017-04-15 12:36:55 -0700

    Build responsive map with temp mock data (us.js)

--- client/containers/data/dataMap.jsx
+++ client/containers/data/dataMap.jsx
@@ -1,38 +1,98 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import axios from 'axios';
 import * as d3 from 'd3';
-import topojson from 'topojson';
+import Faux from 'react-faux-dom'
+import * as topojson from 'topojson';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import QotdList from './pastQotdsList.jsx';
 import '../../styles/index.css';
 
+
+// Resources for responsive map:
+// http://bl.ocks.org/jczaplew/4444770
+// http://eyeseast.github.io/visible-data/2013/08/26/responsive-d3/
 class DataMap extends React.Component {
   constructor() {
     super();
     this.state = {
       question: 'Questions to look up',
-      datamap: null
     };
   }
 
-  renderMap(){
-    return <div class='datamap'></div>
+  sizeChange() {
+    d3.select("g").attr("transform", "scale(" + $("#container").width()/900 + ")");
+    $("svg").height($("#container").width() * 0.618);
+  }
+
+  componentDidMount() {
+    this.sizeChange();
   }
 
   render() {
-    console.log("props in map component: ", this.props.regionData);
-    return (
-      <div id="datamap-container"></div>
-    );
+    console.log("props in map component: ", this.props.stateData);
+    d3.select(window).on('resize', this.sizeChange);
+    
+    var datamapContainer = Faux.createElement('div');   
+    
+    d3.select(datamapContainer)
+      .attr('id', "container");
+    
+    var hoverInfo = d3.select(datamapContainer)
+      .append('div')
+      .attr('id', 'hoverinfo')
+      .classed('hide', true);
+    
+    var svg = d3.select(datamapContainer).append('svg')
+      .attr("width", "100%")
+        .append("g");
+    
+    var projection = d3.geoAlbersUsa()
+      .scale(900);
+    
+    var path = d3.geoPath()
+      .projection(projection);
+    // d3.json('/api/data/q=topojson', function(error, us) {
+    //   console.log(us);
+    // });
+    svg.selectAll('.states')
+      .data(topojson.feature(this.props.mockStateData, this.props.mockStateData.objects.usStates).features)
+      .enter()
+      .append('path')
+      .style('opacity', function(d){
+        return d.properties.unemployment * 0.075;
+      })
+      .style('fill', 'orange')
+      .attr('class', 'states')
+      .attr('d', path)
+      .on('mouseover', function(d){
+        // console.log('d', d);
+        // console.log('this', this);
+        var name = d.properties.STATE_ABBR;
+        var rate = d.properties.unemployment;
+        return d3.select(hoverinfo)
+          .classed('hide', false)
+          .text(name + ' : ' + rate);
+      }) 
+      .on("mousemove", () => {
+        d3.select(hoverinfo)
+          .style("top", (d3.event.pageY-10)+"px")
+          .style("left",(d3.event.pageX+10)+"px");
+      })
+      .on('mouseout', () => {
+        d3.select(hoverinfo)
+          .classed('hide', true)
+      });
+    
+    return datamapContainer.toReact();
   }
 }
 
 function mapStateToProps (state) {
   return {
-    regionData: state.stateDataReducer,
-    stateDefaults: state.stateDefaults
+    stateData: state.stateDataReducer,
+    stateDefaults: state.stateDefaults,
+    mockStateData: state.mockStateData
   };
 }
 
--- client/reducers/reducersIndex.js
+++ client/reducers/reducersIndex.js
@@ -4,13 +4,15 @@ import mockClicked from './mockReducerClicked.js';
 import userReducer from './userReducer.js';
 import stateData from './stateDataReducer.js';
 import stateDefaults from './stateDefaultsReducer.js';
+import mockStates from './us.js';
 
 const allReducers = combineReducers({
   examples: ExampleReducer,
   mockClicked: mockClicked,
   userReducer: userReducer,
   stateDataReducer: stateData,
-  stateDefaults: stateDefaults
+  stateDefaults: stateDefaults,
+  mockStateData: mockStates
 });
 
 export default allReducers;
