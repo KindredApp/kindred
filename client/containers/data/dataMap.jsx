18c955311a6691dc24dcf78ed9217fc9afde457e
Lindsay Anchors <lindsay.anchors@gmail.com>
2017-04-19 15:35:01 -0700

    Fix bug: full map now displays on loading, clean-up comments

--- client/containers/data/dataMap.jsx
+++ client/containers/data/dataMap.jsx
@@ -7,16 +7,11 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import '../../styles/index.css';
 
-  // in dataMap merge the object associated qith requested question with geo data
-    // for each in geometry, if prop.stateabbrev === state abbreviation, set data and continue
-
-// Resources for responsive map:
-// http://bl.ocks.org/jczaplew/4444770
-// http://eyeseast.github.io/visible-data/2013/08/26/responsive-d3/
 class DataMap extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    setTimeout(this.sizeChange, 100);
   }
 
   mergeTopoWithStateData(nextprops) {
@@ -30,27 +25,26 @@ class DataMap extends React.Component {
     this.setState({mergeData: mergeData});
   }
 
-  componentWillReceiveProps(nextprops) {
-    this.mergeTopoWithStateData(nextprops);
-  }
-
   sizeChange() {
-    d3.select("g").attr("transform", "scale(" + $("#mapcontainer").width()/900 + ")");
+    d3.select("g")
+      .attr("transform", "scale(" + $("#mapcontainer").width()/900 + ")");
     $("svg").height($("#mapcontainer").width() * 0.618);
   }
 
   componentDidMount() {
-    this.sizeChange();
+    d3.select(window).on('resize', this.sizeChange);
+  }
+
+  componentWillReceiveProps(nextprops) {
+    this.mergeTopoWithStateData(nextprops);
   }
 
   render() {
     if (this.state.mergeData) {
-      d3.select(window).on('resize', this.sizeChange);
-      
       var datamapContainer = Faux.createElement('div');   
       
       d3.select(datamapContainer)
-        .attr('id', "mapcontainer");
+        .attr('id', "mapcontainer")
 
       var hoverInfo = d3.select(datamapContainer)
         .append('div')
@@ -67,38 +61,38 @@ class DataMap extends React.Component {
       var path = d3.geoPath()
         .projection(projection);
 
-    svg.selectAll('.states')
-      .data(topojson.feature(this.state.mergeData, this.state.mergeData.objects.usStates).features)
-      .enter()
-      .append('path')
-      .style('fill', 'lavender')
-      .style('stroke', 'white')
-      // .style('stroke', 'cornflowerblue')
-      .style('stroke-width', '2px')
-      .attr('class', 'states')
-      .attr('d', path)
-      .on('mouseover', (d) => {
-        var name = d.properties.STATE_ABBR;
-        var data = {total: d.properties.data.total};
-        let total = d.properties.data.total;   
-        let text = `Total: ${d.properties.data.total}\n`;     
-        for (let answer in d.properties.data.answers) {
-          text += `${answer}: ${d.properties.data.answers[answer]}\n`;
-        }
-        return d3.select(hoverinfo)
-          .classed('hide', false)
-          .text(`${name}\n${text}`);
-      }) 
-      .on("mousemove", () => {
-        d3.select(hoverinfo)
-          .style("top", (d3.event.pageY-10)+"px")
-          .style("left",(d3.event.pageX+10)+"px");
-      })
-      .on('mouseout', () => {
-        d3.select(hoverinfo)
-          .classed('hide', true);
-      });
-    return datamapContainer.toReact();
+      svg.selectAll('.states')
+        .data(topojson.feature(this.state.mergeData, this.state.mergeData.objects.usStates).features)
+        .enter()
+        .append('path')
+        .style('fill', 'lavender')
+        .style('stroke', 'white')
+        // .style('stroke', 'cornflowerblue')
+        .style('stroke-width', '2px')
+        .attr('class', 'states')
+        .attr('d', path)
+        .on('mouseover', (d) => {
+          var name = d.properties.STATE_ABBR;
+          var data = {total: d.properties.data.total};
+          let total = d.properties.data.total;   
+          let text = `Total: ${d.properties.data.total}\n`;     
+          for (let answer in d.properties.data.answers) {
+            text += `${answer}: ${d.properties.data.answers[answer]}\n`;
+          }
+          return d3.select(hoverinfo)
+            .classed('hide', false)
+            .text(`${name}\n${text}`);
+        }) 
+        .on("mousemove", () => {
+          d3.select(hoverinfo)
+            .style("top", (d3.event.pageY-10)+"px")
+            .style("left",(d3.event.pageX+10)+"px");
+        })
+        .on('mouseout', () => {
+          d3.select(hoverinfo)
+            .classed('hide', true);
+        });
+      return datamapContainer.toReact();
     } else {
       return null;
     }
