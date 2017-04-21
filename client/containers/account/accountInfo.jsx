7a580e304c8c8a88b16030fe50860a343a1bafa1
Lindsay Anchors <lindsay.anchors@gmail.com>
2017-04-21 00:50:26 -0700

    Display user profile data on account page

--- client/containers/account/accountInfo.jsx
+++ client/containers/account/accountInfo.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import instance from '../../config.js';
-import { Redirect } from 'react-router-dom'; 
+import {Redirect} from 'react-router-dom'; 
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
-import {actionUser} from '../../actions/actionUser.js';
 import {actionSetUserProfile} from '../../actions/actionSetUserProfile.js';
 import {actionSurveyFromAccountPage} from '../../actions/actionSurveyFromAccountPage.js';
 import '../../styles/index.css';
@@ -11,20 +10,23 @@ import '../../styles/index.css';
 class AccountInfo extends React.Component {
   constructor (props) {
     super (props);
-    this.state = {
-      redirectToSurvey: false,
-    };
+    this.state = {};
     this.deleteAccount = this.deleteAccount.bind(this);
     this.editAccount = this.editAccount.bind(this);  
-    // this.props.actionSetUserProfile(____);  
-    // console.log(this.props.userProfileReducer)
+    this._profileToMappableArray = this._profileToMappableArray.bind(this);
+  }
 
+  _profileToMappableArray(profileObj) {
+    let profileArr = [];
+    for (let key in profileObj) {
+      profileArr.push([key, profileObj[key]]);
+    }
+    return profileArr;
   }
 
   deleteAccount() {
     instance.goInstance.delete(`/api/profile?user=${this.props.user.userObj.Username}`)
       .then((response) => {
-        console.log("Response to delete:", response);
       });
   }
 
@@ -34,13 +36,18 @@ class AccountInfo extends React.Component {
   }
 
   render() {
-    // console.log('userobj in account infor: ', this.props.user.userObj);
     return (
-      <div className="landing-container">
-        Delete Account
+      <div className="survey-section">
         { this.props.surveyFromAccountPage ? <Redirect to="/survey" /> : null }
-        <button id="deleteAccount" onClick={this.deleteAccount}>Delete</button>
-        <button id="editAccount" onClick={this.editAccount}>Edit</button>
+        <button id="deleteAccount" onClick={this.deleteAccount}>Delete Account</button>
+        <button id="editAccount" onClick={this.editAccount}>Edit Profile</button>
+        {this._profileToMappableArray(this.props.userProfile).map((field, i) => {
+          return (
+            <div className="review-field" key={field[0]}>
+              <span className="review-field-name">{field[0]}</span>
+              <span className="review-value">{field[1]}</span>
+            </div>);
+        })}
       </div>
     );
   }
@@ -50,7 +57,7 @@ function mapStateToProps (state) {
   return {
     user: state.userReducer,
     surveyFromAccountPage: state.surveyFromAccountPage,
-    userProfileReducer: state.userProfileReducer
+    userProfile: state.userProfileReducer
   };
 }
 
