b268cf33eba1088ea6dfd6f968dd714f86ff5c6e
Zane Ravenholt <zravenholt@gmail.com>
2017-04-21 16:34:25 -0700

    implement loading signal on wait for partner button

--- client/containers/video/video.jsx
+++ client/containers/video/video.jsx
@@ -606,7 +606,7 @@ class Video extends React.Component {
 
     const RoomFoundComponent = (
       <div>
-        {!this.state.roomCount ? <Button type='primary' onClick={this.joinRoom}>Partner found! Click here to chat.</Button> : null}
+        {!this.state.roomCount ? <Button type='primary' loading={true} onClick={this.joinRoom}>Partner found! Click here to chat.</Button> : null}
         {this.state.roomCount ? <Button className="leave-room-button" type='primary' onClick={this.leaveRoom}>Leave room</Button> : null}
         {this.state.roomCount ? <Button className="kinnect-button" type='primary' onClick={this.connectUsers}>Kinnect *TM</Button> : null}
         {this.state.roomInstance === false ? <Redirect to="/video"/> : null}
