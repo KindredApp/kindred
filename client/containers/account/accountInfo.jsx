4cd79bf40389dad47fbd69f07194d6eae0f1d05b
Lindsay Anchors <lindsay.anchors@gmail.com>
2017-04-22 11:42:36 -0700

    Add account deletion notifications

--- client/containers/account/accountInfo.jsx
+++ client/containers/account/accountInfo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import instance from '../../config.js';
-import { Select, Steps, Button} from 'antd';
+import { Select, Steps, Button, notification, message } from 'antd';
 import {Redirect} from 'react-router-dom'; 
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
@@ -11,13 +11,37 @@ import '../../styles/index.css';
 class AccountInfo extends React.Component {
   constructor (props) {
     super (props);
+    this.deleteAccountWarn = this.deleteAccountWarn.bind(this);
     this.deleteAccount = this.deleteAccount.bind(this);
     this.editAccount = this.editAccount.bind(this);  
   }
 
+
+  deleteAccountWarn() {
+    const key = `open${Date.now()}`;
+    const btn = (
+      <Button type="danger" size="small" onClick={()=> {
+        notification.close(key);
+        this.deleteAccount();
+      }}>
+      Delete
+      </Button>
+    );
+    notification.warn({
+      message: 'Are you sure?',
+      description: 'If you delete your account, you will lose your kin contacts and message history.',
+      duration: 8,
+      btn,
+      key,
+      onClose: () => console.log('notifcation closed without deleting account')
+    });
+  }
+
+
   deleteAccount() {
     instance.goInstance.delete(`/api/profile?user=${this.props.user.userObj.Username}`)
       .then((response) => {
+        message.success("Account deleted. If you logout, you will no longer be able to login without creating a new account.", 8, () => console.log('Bye!'));
       });
   }
 
@@ -48,7 +72,7 @@ class AccountInfo extends React.Component {
       <div className="survey-container">
         <div className="steps-section">Account Overview</div>
         <div className="account-btn-section">
-          <button type="button" className="account-btn delete" onClick={this.deleteAccount}><span>Delete Account</span></button>
+          <button type="button" className="account-btn delete" onClick={this.deleteAccountWarn}><span>Delete Account</span></button>
           <button className="account-btn edit" type="button" onClick={this.editAccount}><span>Edit Profile</span></button>
         </div>
         <div className="survey-card">
