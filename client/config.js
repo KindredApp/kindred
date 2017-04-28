f1c7cbdc494c552ddf250808df0f8d3249c89be2
nowreen <chowdhry.n@gmail.com>
2017-04-27 17:49:10 -0700

    Reset server code to deployment version

--- client/config.js
+++ client/config.js
@@ -1,29 +1,15 @@
 import axios from 'axios';
 
-var baseUrlGo;
-var baseUrlNode;
-
-switch (environment) {
-case "dev":
-  baseUrlGo = 'http://localhost:8080';
-  baseUrlNode = 'http://localhost:3000';
-  break;
-case "production":
-  baseUrlGo = 'https://www.kindredchat.io:443';
-  baseUrlNode = 'https://www.kindredchat.io:3000';
-  break;
-default:
-  baseUrlGo = 'http://localhost:8080';
-  baseUrlNode = 'http://localhost:3000';
-  break;
-}
-
 var instance = {
   goInstance: axios.create({
-    baseURL: baseUrlGo
+    //use commented for production build
+    baseURL: 'https://www.kindredchat.io:443'
+    // baseURL: 'http://localhost:8080'
   }),
   nodeInstance: axios.create({
-    baseURL: baseUrlNode
+    //use commented for production build
+    baseURL: 'https://www.kindredchat.io:3000'
+    // baseURL: 'http://localhost:3000'
   })
 };
 
